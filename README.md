9265fc37346028b01ff9d40dd3e681a7ddf69c25
Kenny Armstrong <kenny@linuxacademy.com>
2018-03-22 12:26:34 +0000

    Our first commit for the project.

---
+++ README.md
@@ -0,0 +1,9 @@
+README
+======
+
+A simple readme file to toss into our project.
+
+To compile the code, run:
+
+`g++ -o hi-there hi-there.cpp`
+
---
+++ src/hi-there.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// hi-there.cpp
+// A simple application
+
+using namespace std;
+
+int main()
+{
+	cout << "Hi there!  Welcome to Linux Academy!" << endl;
+	return 0;
+}
+
